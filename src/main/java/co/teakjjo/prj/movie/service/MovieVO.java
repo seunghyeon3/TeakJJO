f1f5588f6403b106788744495ee0418f1fdb1751
백승국 <das01042@naver.com>
2022-01-18 12:09:05 +0900

    'last3'

--- src/main/java/co/teakjjo/prj/movie/service/MovieVO.java
+++ src/main/java/co/teakjjo/prj/movie/service/MovieVO.java
@@ -11,9 +11,9 @@ public class MovieVO {
 	
 	public MovieVO(String rank, String movieAge, String movieTitle, String movieOpenDate) {
 		this.rank = rank;
-		this.movie_grade = movieAge;
-		this.movie_title = movieTitle;
-		this.movie_open = movieOpenDate;
+		this.movieAge = movieAge;
+		this.movieTitle = movieTitle;
+		this.movieOpenDate = movieOpenDate;
 	}
 	public MovieVO(String img) {
 		this.img = img;
@@ -21,15 +21,8 @@ public class MovieVO {
 	
 	String img;
 	String rank;
-	private String movie_id;
-	private String movie_title ;
-	private String movie_open;
-	private String movie_genre ;
-	private String movie_nation;
-	private String movie_grade ;
-	private String movie_person ;
-	private String movie_runningtime ;
-	private String movie_rating;
-	private String movie_pfile ;
-	private String movie_picture;
+	String movieAge;
+	String movieTitle;
+	String movieOpenDate;
+	
 }
